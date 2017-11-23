4bc2d38798a0dd84f4bdfbf8836ad5d3f9615cbc
Blenno Pardim <blenno@live.com>
2017-11-23 13:33:39 -0200

    Endend user endpoint passed in BDD

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = User::all()->toArray();
+        return response()->json($user);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json($user->toArray());
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json($user->toArray());
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return response()->json($user->toArray());
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::findOrFail($id)->delete();
+        return response()->json(['status' => 'Removido com sucesso'], 200);
+    }
+}
--- spec/AppSpec.php
+++ spec/AppSpec.php
@@ -3,6 +3,7 @@
 use Kahlan\Plugin\Monkey;
 use Sofa\LaravelKahlan\Env;
 use Illuminate\Contracts\Foundation\Application;
+use App\User;
 
 describe('Test laravel app', function () 
 {
@@ -14,17 +15,49 @@ describe('Test laravel app', function ()
 describe('Test user endpoint', function() 
 {
  
-  it('create_user /api/users', function() 
-  {
+  it('create_user POST /api/users', function() {
+    
     $user_values = [
       'name' => 'Blenno Cleofas Pardim',
+      'email' => 'blennopardim@gmail.com',
       'cpf' => '03869076178',
+      'password' => '123456789',
       'birthdate' => '1990-08-23'
     ];
     $response = $this->laravel->post('/api/users', $user_values);
     $response->assertStatus(200)
-             ->assertJson($user_values);
+             ->assertJson(['cpf' => $user_values['cpf']]);
   });
 
+  it('get user GET /api/users/{id}', function() {
+    $user = User::where('cpf','=','03869076178')->first();
+    
+    $this->laravel->get('/api/users/'.$user->id)
+            ->assertStatus(200)
+            ->assertJson(['name' => 'Blenno Cleofas Pardim']);
+  });
+
+  it('list all users GET /api/users', function ()
+  {
+      $this->laravel->get('/api/users')
+          ->assertStatus(200);
+  });
+
+  it('edit user put /api/users/{id}', function() {
+    
+    $user = User::where('cpf','=','03869076178')->first();
+    
+    $response = $this->laravel->put('/api/users/'.$user->id , [
+            'name' => 'Blenno Pardim'
+    ]);
+  
+   $response->assertStatus(200)
+                 ->assertJson(['name' => 'Blenno Pardim']);
+  });
+  
+  it('delete user DELETE /api/users', function () {  
+    $user = User::where('cpf','=','03869076178')->first();
+    $this->laravel->delete('/api/users/'.$user->id)->assertStatus(200);
+  });
   
 });
